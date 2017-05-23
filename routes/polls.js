84a921b1d3dda8da0ee7a8d4157d4b540cf9cfcb
James O'Shea <jamesoshea89@hotmail.com>
2017-05-23 15:26:51 +0200

    voting mechanism done
    double votes not caught

--- routes/polls.js
+++ routes/polls.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var Poll = require('../models/poll');
 var User = require('../models/user');
 
+//new poll page
 router.get('/newpoll', ensureAuthenticated,  function(req, res) {
   res.render('newpoll');
 });
@@ -15,6 +16,49 @@ router.get('/all',  function(req, res) {
   });
 });
 
+//show one poll
+router.get('/:id', function(req, res){
+  query = { _id: req.params.id }
+  Poll.findOne(query, function(err, poll){
+    var isOwner = false;
+    if(req.user) {
+      console.log(req.user.id, poll.owner)
+      if (req.user.id == poll.owner) {
+        isOwner = true;
+      }
+    }
+    res.render('poll', {poll: poll, isOwner: isOwner});
+  });
+});
+
+//vote up
+router.get('/:id/:option/voteup', function(req, res){
+  query = { _id: req.params.id }
+  var i = req.params.option;
+  Poll.findOne(query, function(err, poll){
+    poll.options[i].votes++;
+    Poll.findOneAndUpdate(query, { $set: { options: poll.options}, new: true}, function(err, poll){
+      var isOwner = false;
+      if(req.user) {
+        console.log(req.user.id, poll.owner)
+        if (req.user.id == poll.owner) {
+          isOwner = true;
+        }
+      }
+      res.redirect('/polls/' + req.params.id);
+    });
+  });
+});
+
+//delete poll
+router.get('/:id/delete', function(req, res){
+  query = { _id: req.params.id };
+  Poll.remove(query, function(err){
+    if (err) throw err;
+    res.redirect('../../')
+  });
+});
+
 //create poll
 router.post('/newpoll', ensureAuthenticated, function(req, res) {
   var question = req.body.question;
@@ -23,7 +67,6 @@ router.post('/newpoll', ensureAuthenticated, function(req, res) {
   responses.forEach(function(response, index){
     responses[index] = { text: responses[index], votes: 0 };
   });
-  console.log(req.body);
 
   //Validation
   req.checkBody('question', 'Name is required').notEmpty();
@@ -42,7 +85,6 @@ router.post('/newpoll', ensureAuthenticated, function(req, res) {
 
     newPoll.save(function(err) {
       if (err) throw err;
-      console.log(newPoll);
     });
 
     req.flash('success_msg', 'Poll created.');
