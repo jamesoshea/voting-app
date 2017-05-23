b40e9fa8b635b1f77d3a306dcd920136b8884c99
James O'Shea <jamesoshea89@hotmail.com>
2017-05-23 08:53:27 +0200

    boilerplate code removed lol

--- models/genmodel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-//create a schema for mongoose to play with
-var GenericSchema = mongoose.Schema({
-	name: String
-});
-
-//export it
-var Generic = module.exports = mongoose.model('Generic', GenericSchema);
